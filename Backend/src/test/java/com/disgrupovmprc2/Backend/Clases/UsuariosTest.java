a86355755d71c770bd1e7c805f0552ddd243ec3c
Veronica <veronicaciancia@gmail.com>
2022-06-22 13:11:41 +0200

    UsuarioTest

---
+++ Backend/src/test/java/com/disgrupovmprc2/Backend/Clases/UsuariosTest.java
@@ -0,0 +1,24 @@
+package com.disgrupovmprc2.Backend.Clases;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UsuariosTest {
+
+    @Test
+    public void getEmail() {
+    }
+
+    @Test
+    public void getTelefono() {
+    }
+
+    @Test
+    public void getUbicacion() {
+    }
+
+    @Test
+    public void getDepartamento() {
+    }
+}
