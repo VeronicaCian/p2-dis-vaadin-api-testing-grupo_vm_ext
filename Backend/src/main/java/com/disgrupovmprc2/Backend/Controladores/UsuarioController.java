14a62cff61f0d64eab3e5d4feb93b7a8d3f8c2c1
Veronica <veronicaciancia@gmail.com>
2022-06-22 10:05:43 +0200

    DeleteMapping y GetMapping-UsuarioController

--- Backend/src/main/java/com/disgrupovmprc2/Backend/Controladores/UsuarioController.java
+++ Backend/src/main/java/com/disgrupovmprc2/Backend/Controladores/UsuarioController.java
@@ -114,4 +114,74 @@ public class UsuarioController {
         return new ResponseEntity<>(HttpStatus.CREATED);
 
     }
+    //para buscar por id cojo el ide del elemento encontrado
+    @DeleteMapping(value = "/usuario/{id}")
+    public ResponseEntity deleteUser (@PathVariable("id") int id) throws IOException{
+
+
+        Boolean encontrado = false;
+        Usuarios user = null;
+        int identificador = id;
+
+        ArrayList<Usuarios> listamarcadores = utils.lecturaJSONUsuarios();
+
+        int i = 0;
+        while(!encontrado && i < listamarcadores.size()){
+            Usuarios u = listamarcadores.get(i);
+            if(identificador == u.getId()){
+                encontrado = true;
+                user = u;
+                listamarcadores.remove(user);
+            }
+            i++;
+        }
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create(); //hacmeos que aparezca bonito en vez de en una linea
+        FileWriter writer = null; //inicializamos el filewriter
+        String archivo = "Usuarios.json";
+
+        try{
+            writer = new FileWriter(archivo);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        gson.toJson(listamarcadores,writer);
+
+        try {
+            writer.close();
+        } catch (IOException e2) {
+            e2.printStackTrace();
+        }
+
+        return new ResponseEntity(HttpStatus.ACCEPTED);
+
+    }
+
+    //para buscar por id cojo el ide del elemento encontrado
+    @GetMapping(value = "/usuarios/{id}")
+    public Usuarios buscarUsuarioID (@PathVariable int id) throws IOException{
+
+
+        Boolean encontrado = false;
+        Usuarios user = null;
+        int identificador = id;
+
+        ArrayList<Usuarios> listausuarios = utils.lecturaJSONUsuarios();
+
+        int i = 0;
+        while(!encontrado && i < listausuarios.size()){
+            Usuarios u = listausuarios.get(i);
+            if(identificador == u.getId()){
+                encontrado = true;
+                user = u;
+            }
+            i++;
+        }
+        
+        return user;
+
+    }
+
+
 }
