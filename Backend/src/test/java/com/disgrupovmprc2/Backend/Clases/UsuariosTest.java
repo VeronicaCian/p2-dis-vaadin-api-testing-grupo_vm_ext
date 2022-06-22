2112e4d03538a91b05a3a529b4d9a6bee40a2809
Veronica <veronicaciancia@gmail.com>
2022-06-22 13:41:59 +0200

    UsuarioTests

--- Backend/src/test/java/com/disgrupovmprc2/Backend/Clases/UsuariosTest.java
+++ Backend/src/test/java/com/disgrupovmprc2/Backend/Clases/UsuariosTest.java
@@ -8,17 +8,65 @@ public class UsuariosTest {
 
     @Test
     public void getEmail() {
+        Usuarios usu = new Usuarios(
+                5,
+                "Pablo",
+                "Publicidad",
+                "Madrid",
+                648039974,
+                "pablonf@gmail.com");
+        assertTrue(usu.getEmail().contains("@"));
     }
 
     @Test
     public void getTelefono() {
+        Usuarios usu = new Usuarios(
+                5,
+                "Miguel",
+                "Contabilidad",
+                "Pozuelo",
+                654783985,
+                "miguelcont@gmail.com");
+        assertTrue(usu.getTelefono()>99999999 && usu.getTelefono()<999999999);
     }
 
     @Test
     public void getUbicacion() {
+        Usuarios usu = new Usuarios(
+                5,
+                "Miguel",
+                "Contabilidad",
+                "Pozuelo",
+                654783985,
+                "miguelcont@gmail.com");
+
+        assertFalse("ERROR, Ubicación vacio", usu.getUbicacion().isEmpty());
+
     }
 
     @Test
     public void getDepartamento() {
+        Usuarios usu = new Usuarios(
+                5,
+                "Manuel",
+                "Contabilidad",
+                "Pozuelo",
+                654783985,
+                "miguelcont@gmail.com");
+
+        assertFalse("ERROR, Departamento vacio", usu.getDepartamento().isEmpty());
     }
+    @Test
+    public void getNombre() {
+        Usuarios usu = new Usuarios(
+                5,
+                "Manuel",
+                "Contabilidad",
+                "Pozuelo",
+                654783985,
+                "miguelcont@gmail.com");
+
+        assertFalse("ERROR, Nombre vacio", usu.getNombre().isEmpty());
+    }
+
 }
